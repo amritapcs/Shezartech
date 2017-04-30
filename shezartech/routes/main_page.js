4c706e38ae7eb66de336fd8d8b178d5736491c55
himanshu <himanshu.yadav@sendinblue.com>
2017-04-30 13:24:32 +0530

    a

--- shezartech/routes/main_page.js
+++ shezartech/routes/main_page.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-console.log("kkkkkk")
-
-/* GET Signup page. */
+/* GET main page page. */
 router.get('/', function(req, res, next) {
-	res.render('main_page', { title: 'Main_page' });
+	res.send('respond with a resourceaaaa');
 });
 
 module.exports = router;
