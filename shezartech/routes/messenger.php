a25a74e2924a17409188aa4f0ea7be4432378e8a
himanshu <himanshu.yadav@sendinblue.com>
2017-04-30 16:49:43 +0530

    a

---
+++ shezartech/routes/messenger.php
@@ -0,0 +1,28 @@
+<?php
+    
+    echo "One";
+    function my_function_zalo($oaid, $data, $timestamp, $secretkey){
+
+        //$datas = json_encode($data);
+    	$passcode = (int)$oaid.$data.$timestamp.$secretkey;
+
+    	echo $passcode;
+
+        //$passcode = '1032900368143269705{"phone":841289456817,"templateid":"cc78f992c5d72c8975c6","templatedata":{"name":"Amrita","company":"Shezartech","number":"123","date":"01/01/2017"}}1492597209077IEklE4N1I7bWqp5TOQ2F';
+        
+        $result = hash('sha256', $passcode);
+        return $result;
+    }
+
+    function my_function($oaid, $sender, $text, $timestamp, $secretkey){
+        //echo $sender;
+        $arr = array('uid'=>(int)$sender,'message'=>$text);
+        $data = json_encode($arr);
+        //echo $data;
+        $passcode = (int)$oaid.$data.$timestamp.$secretkey;
+        echo $passcode;
+        
+        $result = hash('sha256', $passcode);
+        return $result;
+    }
+?>
