b3a506fb2158c1f19ae7a005efb239f8ee2e2a6d
himanshu <himanshu.yadav@sendinblue.com>
2017-05-16 22:09:17 +0530

    a

--- shezartech/routes/form_contact.js
+++ shezartech/routes/form_contact.js
@@ -76,6 +76,7 @@ router.post('/', function (req, res, next) {
 		                              	body = JSON.parse(body)
 		                                if(body.data) {
 		                                	var msgid = body.body.msgId;
+		                                	console.log("msgid is :: "+msgid)
 		                                    var insert_data = {templateid: templateid, phone:phone, time : timestamp, delivery_status : body.data.status, msgid : msgid }
 		                                    db.collection('delivery_status').update({ fromuid:fromuid, msgid:msgid }, insert_data, { upsert : true });
 		                                }
