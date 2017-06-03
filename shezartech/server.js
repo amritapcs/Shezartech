db93e8673ec42671f7e49642b565c45a28d64fad
himanshu <himanshu.yadav@sendinblue.com>
2017-06-03 22:16:06 +0530

    a

--- shezartech/server.js
+++ shezartech/server.js
@@ -43,7 +43,7 @@ console.log("himanshu yadav")
 var builder = require('botbuilder');
 
 //var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3000, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
